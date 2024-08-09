88ddc64d43239f3aad47c96f7a7ba23d60529b46
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-08-09 13:19:59 +0100

    secrets taken care of

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKvZZ8GtRd3RW1yqiQzDdiAGY2M3eUdag",
-  authDomain: "pantry-tracker-app-e7bf9.firebaseapp.com",
-  projectId: "pantry-tracker-app-e7bf9",
-  storageBucket: "pantry-tracker-app-e7bf9.appspot.com",
-  messagingSenderId: "292202806642",
-  appId: "1:292202806642:web:a1da10b69b7d94ccd771eb",
-  measurementId: "G-S2MNBRPZYJ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
